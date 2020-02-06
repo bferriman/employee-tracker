150dcb663ebc7670a1f97134b8cbf57d8bb14503
Ben F <56423163+bferriman@users.noreply.github.com>
2020-02-05 23:29:17 -0500

    added foreign keys and constraints

--- schema.sql
+++ schema.sql
@@ -3,22 +3,35 @@ CREATE database companyDB;
 
 USE companyDB;
 
-CREATE TABLE employee (
+CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INT NULL,
-  manager_id INT NULL
+  name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
-  salary DEC(12,2) NULL,
-  department_id INT NULL
+  salary DEC(12,2) NOT NULL,
+  department_id INT NOT NULL,
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE
 );
 
-CREATE TABLE department (
+CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT NULL,
+  FOREIGN KEY (role_id)
+  REFERENCES role(id)
+  ON DELETE RESTRICT
+  ON UPDATE CASCADE,
+  FOREIGN KEY (manager_id)
+  REFERENCES employee(id)
+  ON DELETE SET NULL
+  ON UPDATE CASCADE
 );
+
