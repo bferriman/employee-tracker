acdefc9e30b80124418991e6b4f697efc48c8a20
Ben F <56423163+bferriman@users.noreply.github.com>
2020-02-07 21:38:46 -0500

    more work

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+const cTable = require('console.table');
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -10,25 +11,27 @@ var connection = mysql.createConnection({
 });
 
 connection.connect( err => {
-  if (err) throw err;
+  if (err) throw err;  
+  console.log("*******************");
+  console.log("Welcome to Employee Tracker!");
   showDepartments();
 });
 
 
 function showDepartments() {
-  console.log("Showing departments");
+  console.log("*******************");
   const query = "SELECT name FROM department";
   connection.query(query, (err, res) => {
     if (err) throw err;
-    console.log(res);
+    console.table("Departments", res);
+    console.log("-------------------");
     const choices = res.map(dept => dept.name);
     choices.push("Add a department", "Show all employees", "Exit");
-    console.log(choices);
     inquirer
     .prompt({
       name: "action",
       type: "list",
-      message: "Departments:",
+      message: "Select:",
       choices: choices
     })
     .then( ({ action }) => {
@@ -53,22 +56,13 @@ function showDepartments() {
   });
 }
 
-function addDepartment() {
-  console.log("Adding a department");
-  showDepartments();
-}
-
-function deleteDepartment(dept) {
-  console.log("Deleting department: " + dept);
-  showDepartments();
-}
-
 function showRoles(department) {
-  console.log("Showing roles for " + department);
+  console.log("*******************");
   const query = "SELECT title, salary FROM role INNER JOIN department on role.department_id = department.id WHERE department.name = ?";
   connection.query(query, [department], (err, res) => {
     if (err) throw err;
-    console.log(res);
+    console.table(`${department} Roles`, res);
+    console.log("-------------------");
     const choices = res.map(role => role.title);
     choices.push("Add a role", "Show department employees", "Show department payroll", "Delete department", "Exit");
 
@@ -76,7 +70,7 @@ function showRoles(department) {
     .prompt({
       name: "action",
       type: "list",
-      message: `${department} Roles:`,
+      message: "Select:",
       choices: choices
     })
     .then( ({ action }) => {
@@ -108,17 +102,84 @@ function showRoles(department) {
   });
 }
 
+function showEmployeesByRole(role) {
+  console.log("*******************");
+  const query = "SELECT first_name, last_name FROM employee INNER JOIN role on employee.role_id = role.id WHERE role.title = ?";
+  connection.query(query, [department], (err, res) => {
+    if (err) throw err;
+    console.table(`${department} Roles`, res);
+    console.log("-------------------");
+    const choices = res.map(role => role.title);
+    choices.push("Add a role", "Show department employees", "Show department payroll", "Delete department", "Exit");
+
+    inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "Select:",
+      choices: choices
+    })
+    .then( ({ action }) => {
+      switch (action) {  
+        case "Add a role":
+          addRole();
+          break;
+    
+        case "Show department employees":
+          showDeptEmployees(department);
+          break;
+      
+        case "Show department payroll":
+          showPayroll(department);
+          break;
+
+        case "Delete department":
+          deleteDepartment(department);
+
+        case "Exit":
+          connection.end();
+          break;
+
+        default:
+          showEmployeesByRole(action);
+          break;
+      }
+    });
+  });
+}
+
+
+function addDepartment() {
+  console.log("Adding a department");
+  showDepartments();
+}
+
+function addRole() {
+  console.log("Adding a role");
+  showDepartments();
+}
+
+function deleteDepartment(dept) {
+  console.log("Deleting department: " + dept);
+  showDepartments();
+}
+
 function showAllEmployees() {
   console.log("Showing all employees");
   showDepartments();
 }
 
-function showEmployeesByRole(role) {
-  console.log("Showing " + role + "s");
+function showDeptEmployees(dept) {
+  console.log(`Showing employees for ${dept}`);
   showDepartments();
 }
 
 function showEmployeesByManager(manager) {
   console.log("Showing " + manager + "'s employees");
+  showDepartments();
+}
+
+function showPayroll(dept) {
+  console.log(`Showing payroll for ${dept}`);
   showDepartments();
 }
